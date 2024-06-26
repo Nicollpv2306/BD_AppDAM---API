4b5a9656dfdf5f60ae10d842c7232a8116f73dc2
Nicoll <105988853+Nicollpv2306@users.noreply.github.com>
2024-06-26 02:37:22 -0500

    BD modificado para nuevo tema de proyecto

--- BD_AppDAM.sql
+++ BD_AppDAM.sql
@@ -4,345 +4,170 @@ Use BD_AppDAM;
 
 
 -----CREACION DE TABLAS--------------
--- Crear tabla Usuarios
-CREATE TABLE Usuarios (
-    id_usuario INT PRIMARY KEY IDENTITY,
-    nombre VARCHAR(50),
-    apellido VARCHAR(50),
-    correo VARCHAR(100),
-    contrasenia VARCHAR(100),
-    tipo_usuario VARCHAR(20) -- Puede ser 'administrador', 'miembro', etc.
+-- Crear tabla LoginApp
+CREATE TABLE LoginApp (
+    id_login int primary key identity,
+	usuario VARCHAR(50),
+	contrasenia VARCHAR(50)
 );
 
-select*from Usuarios;
+select*from LoginApp;
 
--- Crear tabla Autores
-CREATE TABLE Autores (
-    id_autor INT PRIMARY KEY IDENTITY,
-    nombre VARCHAR(50),
-    apellido VARCHAR(50)
+-- Crear tabla Categoria
+CREATE TABLE Categoria (
+    id_categoria int primary key identity,
+	nombre VARCHAR(80),
+	descripcion VARCHAR(80)
+);
+select*from Categoria;
+
+-- Crear tabla Producto
+CREATE TABLE Producto (
+    id_producto int primary key identity,
+	nombre VARCHAR(80),
+	precio decimal (10,2),
+	descripcion VARCHAR(80),
+	foto VARCHAR(80),
+	id_categoria int,
+	Foreign key (id_categoria) references Categoria(id_categoria)
 );
 
-select*from Autores;
+select*from Producto;
 
--- Crear tabla Editoriales
-CREATE TABLE Editoriales (
-    id_editorial INT PRIMARY KEY IDENTITY,
-    nombre VARCHAR(100),
-    direccion VARCHAR(255),
-    correo VARCHAR(100)
+-- Crear tabla Pedido
+CREATE TABLE Pedido (
+    id_pedido int primary key identity,
+	nom_cli VARCHAR(80),
+	num_mesa int,
+	detalle VARCHAR(80),
+	precio decimal (10,2),
+	id_producto int,
+	Foreign key (id_producto) references Producto(id_producto)
 );
 
-select*from Editoriales;
+select*from Pedido;
 
--- Crear tabla Categorias
-CREATE TABLE Categorias (
-    id_categoria INT PRIMARY KEY IDENTITY,
-    nombre VARCHAR(50)
-);
-
-select*from Categorias;
-
--- Crear tabla Estado
-CREATE TABLE Estado (
-    id_estado INT PRIMARY KEY IDENTITY,
-    tipo_estado VARCHAR(50) -- Puede ser 'prestado limitadamente', 'comprado', etc.
-);
-
-select*from Estado;
-
--- Crear tabla Libros
-CREATE TABLE Libros (
-    id_libro INT PRIMARY KEY IDENTITY,
-    titulo VARCHAR(200),
-    id_autor INT,
-    id_editorial INT,
-    descripcion VARCHAR(200),
-    imagen VARCHAR(255), -- URL de la imagen
-    id_categoria INT,
-    id_estado INT,
-    FOREIGN KEY (id_autor) REFERENCES Autores(id_autor),
-    FOREIGN KEY (id_editorial) REFERENCES Editoriales(id_editorial),
-    FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria),
-    FOREIGN KEY (id_estado) REFERENCES Estado(id_estado)
-);
-
-select*from Libros;
 
 -------INSERTAR REGISTROS--------
------- Insertar registros en tabla Usuarios
-Insert into Usuarios(nombre, apellido, correo, contrasenia, tipo_usuario)
-values('Juan', 'Perez', 'juanp@hotmail.com', 'juanp123', 'Empleado');
+------ Insertar registros en tabla LoginApp
+Insert into LoginApp(usuario, contrasenia)
+values('miguel','miguel23');
+Go
+Insert into LoginApp(usuario, contrasenia)
+values('juan','juan123');
 Go
 
-Insert into Usuarios(nombre, apellido, correo, contrasenia, tipo_usuario)
-values('Mario', 'Gomez', 'mariog@hotmail.com', 'mariog123', 'Administrador');
+------ Insertar registros en tabla Categoria
+Insert into Categoria(nombre, descripcion)
+values('Entradas', 'Platos de entrada');
+Go
+Insert into Categoria(nombre, descripcion)
+values('Postres', 'Postres diversos');
 Go
 
------- Insertar registros en tabla Autores
-Insert into Autores(nombre, apellido)
-values('Dante', 'Alighieri')
-Go;
+------ Insertar registros en tabla Producto
+Insert into Producto(nombre, precio, descripcion, foto, id_categoria)
+values('Sopa 1', 12.50, 'Sopa n° 1', 'sopa1.jpg', 1);
+Go
+Insert into Producto(nombre, precio, descripcion, foto, id_categoria)
+values('Postre 1', 7.90, 'Postre n° 1', 'postre1.jpg', 2);
+Go
 
-Insert into Autores(nombre, apellido)
-values('Margaret', 'Mitchell')
-Go;
+------ Insertar registros en tabla Pedido
+Insert into Pedido(nom_cli, num_mesa, detalle, precio, id_producto)
+values('Cliente1', 1, 'Pedido n° 1', 12.50, 1);
+Go
+Insert into Pedido(nom_cli, num_mesa, detalle, precio, id_producto)
+values('Cliente2', 2, 'Pedido n° 2', 7.90, 2);
+Go
 
------- Insertar registros en tabla Editoriales
-Insert into Editoriales(nombre, direccion, correo)
-values('Edimat Libros', 'Madrid, España', 'edimat@edimat.es')
-Go;
-
-Insert into Editoriales(nombre, direccion, correo)
-values('Macmillan Publishers', 'Londres, Reino Unido', 'press.inquiries@macmillan.com')
-Go;
-
------- Insertar registros en tabla Categorias
-Insert into Categorias(nombre)
-values('Poesía')
-Go;
-
-Insert into Categorias(nombre)
-values('Novela')
-Go;
-
------- Insertar registros en tabla Estado
-Insert into Estado(tipo_estado)
-values('Vendido')
-Go;
-
-Insert into Estado(tipo_estado)
-values('Prestado')
-Go;
-
------- Insertar registros en tabla Libros
-------Con la API finalizada
 
 ------PROCEDIMIENTOS ALMACENADOS--------------
-------SP para tabla Usuarios
-CREATE PROCEDURE usp_listaUsuarios
+------SP para tabla LoginApp
+CREATE PROCEDURE usp_listaLoginApp
 AS
 BEGIN
-    SELECT id_usuario, nombre, apellido, correo, contrasenia, tipo_usuario
-	FROM Usuarios
+    SELECT id_login, usuario, contrasenia
+	FROM LoginApp
 END;
 GO
 
-EXEC usp_listaUsuarios;
+EXEC usp_listaLoginApp;
 GO
 
-CREATE PROCEDURE usp_obtenerUsuarioPorId
-@id_usuario int
+CREATE PROCEDURE usp_obtenerLoginAppPorId
+@id_login int
 AS
 BEGIN
-    SELECT id_usuario, nombre, apellido, correo, contrasenia, tipo_usuario
-	FROM Usuarios
-	WHERE id_usuario=@id_usuario
+    SELECT id_login, usuario, contrasenia
+	FROM LoginApp
+	WHERE id_login=@id_login
 END;
 GO
 
-EXEC usp_obtenerUsuarioPorId 3;
+EXEC usp_obtenerLoginAppPorId 1;
 GO
 
-CREATE PROCEDURE usp_addUsuario
-@nombre VARCHAR(50),
-@apellido VARCHAR(50),
-@correo VARCHAR(100),
-@contrasenia VARCHAR(100),
-@tipo_usuario VARCHAR(20)
+CREATE PROCEDURE usp_addLoginApp
+@usuario VARCHAR(50),
+@contrasenia VARCHAR(50)
+
 AS
 BEGIN
-    INSERT INTO Usuarios(nombre, apellido, correo, contrasenia, tipo_usuario)
-	VALUES (@nombre, @apellido, @correo, @contrasenia, @tipo_usuario)
+    INSERT INTO LoginApp(usuario, contrasenia)
+	VALUES (@usuario, @contrasenia)
 END;
 GO
 
-EXEC usp_addUsuario 'Pepe', 'Mago', 'pepem@gmail.com', 'pepem123', 'Administrador';
+EXEC usp_addLoginApp 'pedrito', 'pedrito123';
 GO
 
-CREATE PROCEDURE usp_updateUsuario
-@id_usuario INT,
-@nombre VARCHAR(50),
-@apellido VARCHAR(50),
-@correo VARCHAR(100),
-@contrasenia VARCHAR(100),
-@tipo_usuario VARCHAR(20)
+CREATE PROCEDURE usp_updateLoginApp
+@id_login int, 
+@usuario VARCHAR(50), 
+@contrasenia VARCHAR(50)
 AS
 BEGIN
-    UPDATE Usuarios
-	SET nombre = @nombre, apellido = @apellido, correo = @correo, contrasenia = @contrasenia, tipo_usuario = @tipo_usuario
-	WHERE id_usuario = @id_usuario
+    UPDATE LoginApp
+	SET usuario = @usuario, contrasenia = @contrasenia
+	WHERE id_login = @id_login
 END;
 GO
 
-EXEC usp_updateUsuario 3, 'Tito', 'Pana', 'titop@gmail.com', 'titop123', 'Maestro';
+EXEC usp_updateLoginApp 3, 'tito', 'tito123';
 GO
 
-CREATE PROCEDURE usp_deleteUsuario
-@id_usuario int
+CREATE PROCEDURE usp_deleteLoginApp
+@id_login int
 AS
 BEGIN
     DELETE
-	FROM Usuarios
-	WHERE id_usuario=@id_usuario
+	FROM LoginApp
+	WHERE id_login=@id_login
 END;
 GO
 
-EXEC usp_deleteUsuario 3;
+EXEC usp_deleteLoginApp 3;
 GO
 
-------SP para tabla Autores
-CREATE PROCEDURE usp_listaAutores
+------SP para tabla Categoria
+CREATE PROCEDURE usp_listaCategoria
 AS
 BEGIN
-    SELECT id_autor, nombre, apellido
-	FROM Autores
+    SELECT id_categoria, nombre, descripcion
+	FROM Categoria
 END;
 GO
 
-EXEC usp_listaAutores;
-GO
-
-CREATE PROCEDURE usp_obtenerAutorPorId
-@id_autor int
-AS
-BEGIN
-	SELECT id_autor, nombre, apellido
-	FROM Autores
-	WHERE id_autor=@id_autor
-END;
-GO
-
-EXEC usp_obtenerAutorPorId 3;
-GO
-
-CREATE PROCEDURE usp_addAutor
-@nombre VARCHAR(50),
-@apellido VARCHAR(50)
-AS
-BEGIN
-    INSERT INTO Autores(nombre, apellido)
-	VALUES (@nombre, @apellido)
-END;
-GO
-
-EXEC usp_addAutor 'Autor3', 'ApeAutor3';
-GO
-
-CREATE PROCEDURE usp_updateAutor
-@id_autor INT,
-@nombre VARCHAR(50),
-@apellido VARCHAR(50)
-AS
-BEGIN
-    UPDATE Autores
-	SET nombre = @nombre, apellido = @apellido
-	WHERE id_autor = @id_autor
-END;
-GO
-
-EXEC usp_updateAutor 3, 'NomAutor3', 'ApeAutor3';
-GO
-
-CREATE PROCEDURE usp_deleteAutor
-@id_autor int
-AS
-BEGIN
-    DELETE
-	FROM Autores
-	WHERE id_autor=@id_autor
-END;
-GO
-
-EXEC usp_deleteAutor 3;
-GO
-
-------SP para tabla Editoriales
-CREATE PROCEDURE usp_listaEditoriales
-AS
-BEGIN
-    SELECT id_editorial, nombre, direccion, correo
-	FROM Editoriales
-END;
-GO
-
-EXEC usp_listaEditoriales;
-GO
-
-CREATE PROCEDURE usp_obtenerEditorialPorId
-@id_editorial int
-AS
-BEGIN
-	SELECT id_editorial, nombre, direccion, correo
-	FROM Editoriales
-	WHERE id_editorial=@id_editorial
-END;
-GO
-
-EXEC usp_obtenerEditorialPorId 3;
-GO
-
-CREATE PROCEDURE usp_addEditorial
-@nombre VARCHAR(100),
-@direccion VARCHAR(255),
-@correo VARCHAR(100)
-AS
-BEGIN
-    INSERT INTO Editoriales(nombre, direccion, correo)
-	VALUES (@nombre, @direccion, @correo)
-END;
-GO
-
-EXEC usp_addEditorial 'Editorial', 'DirecEditorial', 'correoEditorial';
-GO
-
-CREATE PROCEDURE usp_updateEditorial
-@id_editorial INT,
-@nombre VARCHAR(100),
-@direccion VARCHAR(255),
-@correo VARCHAR(100)
-AS
-BEGIN
-    UPDATE Editoriales
-	SET nombre = @nombre, direccion = @direccion, correo = @correo
-	WHERE id_editorial = @id_editorial
-END;
-GO
-
-EXEC usp_updateEditorial 3, 'Editorial3', 'DireccEditorial3', 'CorreoEditorial3';
-GO
-
-CREATE PROCEDURE usp_deleteEditorial
-@id_editorial int
-AS
-BEGIN
-    DELETE
-	FROM Editoriales
-	WHERE id_editorial=@id_editorial
-END;
-GO
-
-EXEC usp_deleteEditorial 3;
-GO
-
-------SP para tabla Categorias
-CREATE PROCEDURE usp_listaCategorias
-AS
-BEGIN
-    SELECT id_categoria, nombre
-	FROM Categorias
-END;
-GO
-
-EXEC usp_listaCategorias;
+EXEC usp_listaCategoria;
 GO
 
 CREATE PROCEDURE usp_obtenerCategoriaPorId
 @id_categoria int
 AS
 BEGIN
-	SELECT id_categoria, nombre
-	FROM Categorias
+	SELECT id_categoria, nombre, descripcion
+	FROM Categoria
 	WHERE id_categoria=@id_categoria
 END;
 GO
@@ -351,37 +176,39 @@ EXEC usp_obtenerCategoriaPorId 1;
 GO
 
 CREATE PROCEDURE usp_addCategoria
-@nombre VARCHAR(50)
+@nombre VARCHAR(80),
+@descripcion VARCHAR(80)
 AS
 BEGIN
-    INSERT INTO Categorias(nombre)
-	VALUES (@nombre)
+    INSERT INTO Categoria(nombre, descripcion)
+	VALUES (@nombre, @descripcion)
 END;
 GO
 
-EXEC usp_addCategoria 'Infantil';
+EXEC usp_addCategoria 'Ensaladas', 'Ensaladas diversas';
 GO
 
 CREATE PROCEDURE usp_updateCategoria
 @id_categoria INT,
-@nombre VARCHAR(50)
+@nombre VARCHAR(80),
+@descripcion VARCHAR(80)
 AS
 BEGIN
-    UPDATE Categorias
-	SET nombre = @nombre
+    UPDATE Categoria
+	SET nombre = @nombre, descripcion = @descripcion
 	WHERE id_categoria = @id_categoria
 END;
 GO
 
-EXEC usp_updateCategoria 3, 'Comedia';
+EXEC usp_updateCategoria 3, 'Acompañamientos', 'Platos de acompañamiento';
 GO
 
 CREATE PROCEDURE usp_deleteCategoria
-@id_categoria INT
+@id_categoria int
 AS
 BEGIN
     DELETE
-	FROM Categorias
+	FROM Categoria
 	WHERE id_categoria=@id_categoria
 END;
 GO
@@ -389,143 +216,147 @@ GO
 EXEC usp_deleteCategoria 3;
 GO
 
-------SP para tabla Estado
-CREATE PROCEDURE usp_listaEstado
+------SP para tabla Producto
+CREATE PROCEDURE usp_listaProducto
 AS
 BEGIN
-    SELECT id_estado, tipo_estado
-	FROM Estado
+    SELECT id_producto, nombre, precio, descripcion, foto, id_categoria
+	FROM Producto
 END;
 GO
 
-EXEC usp_listaEstado;
+EXEC usp_listaProducto;
 GO
 
-CREATE PROCEDURE usp_obtenerEstadoPorId
-@id_estado int
+CREATE PROCEDURE usp_obtenerProductoPorId
+@id_producto int
 AS
 BEGIN
-	SELECT id_estado, tipo_estado
-	FROM Estado
-	WHERE id_estado=@id_estado
+	SELECT id_producto, nombre, precio, descripcion, foto, id_categoria
+	FROM Producto
+	WHERE id_producto=@id_producto
 END;
 GO
 
-EXEC usp_obtenerEstadoPorId 1;
+EXEC usp_obtenerProductoPorId 1;
 GO
 
-CREATE PROCEDURE usp_addEstado
-@tipo_estado VARCHAR(50)
+CREATE PROCEDURE usp_addProducto
+@nombre VARCHAR(80),
+@precio decimal (10,2),
+@descripcion VARCHAR(80),
+@foto VARCHAR(80),
+@id_categoria int
+
 AS
 BEGIN
-    INSERT INTO Estado(tipo_estado)
-	VALUES (@tipo_estado)
+    INSERT INTO Producto(nombre, precio, descripcion, foto, id_categoria)
+	VALUES (@nombre, @precio, @descripcion, @foto, @id_categoria)
 END;
 GO
 
-EXEC usp_addEstado 'Pendiente';
+EXEC usp_addProducto 'Sopa 2', 15.30, 'Sopa n° 2', 'sopa2.jpg', 1;
 GO
 
-CREATE PROCEDURE usp_updateEstado
-@id_estado INT,
-@tipo_estado VARCHAR(50)
+CREATE PROCEDURE usp_updateProducto
+@id_producto INT,
+@nombre VARCHAR(80),
+@precio decimal (10,2),
+@descripcion VARCHAR(80),
+@foto VARCHAR(80),
+@id_categoria int
 AS
 BEGIN
-    UPDATE Estado
-	SET tipo_estado = @tipo_estado
-	WHERE id_estado = @id_estado
+    UPDATE Producto
+	SET nombre = @nombre, precio = @precio, descripcion = @descripcion, foto = @foto, id_categoria = @id_categoria
+	WHERE id_producto = @id_producto
 END;
 GO
 
-EXEC usp_updateEstado 3, 'Reservado';
+EXEC usp_updateProducto 3, 'Postre 2', 20.50, 'Postre n° 2', 'postre2.jpg', 2 ;
 GO
 
-CREATE PROCEDURE usp_deleteEstado
-@id_estado int
-AS
-BEGIN
-    DELETE
-	FROM Estado
-	WHERE id_estado=@id_estado
-END;
-GO
-
-EXEC usp_deleteEstado 3;
-GO
-
-------SP para tabla Libros
-CREATE PROCEDURE usp_listaLibros
-AS
-BEGIN
-    SELECT id_libro, titulo, id_autor, id_editorial, descripcion, imagen, id_categoria, id_estado 
-	FROM Libros
-END;
-GO
-
-EXEC usp_listaLibros;
-GO
-
-CREATE PROCEDURE usp_obtenerLibroPorId
-@id_libro int
-AS
-BEGIN
-	SELECT id_libro, titulo, id_autor, id_editorial, descripcion, imagen, id_categoria, id_estado 
-	FROM Libros
-	WHERE id_libro=@id_libro
-END;
-GO
-
-EXEC usp_obtenerLibroPorId 1;
-GO
-
-CREATE PROCEDURE usp_addLibro
-@titulo VARCHAR(200),
-@id_autor INT,
-@id_editorial INT,
-@descripcion VARCHAR(200),
-@imagen VARCHAR(255),
-@id_categoria INT,
-@id_estado INT
-AS
-BEGIN
-    INSERT INTO Libros(titulo,id_autor, id_editorial, descripcion, imagen, id_categoria, id_estado)
-	VALUES (@titulo, @id_autor, @id_editorial, @descripcion, @imagen, @id_categoria, @id_estado)
-END;
-GO
-
---EXEC usp_addLibro ''; 
---GO
-
-CREATE PROCEDURE usp_updateLibro
-@id_libro INT,
-@titulo VARCHAR(200),
-@id_autor INT,
-@id_editorial INT,
-@descripcion VARCHAR(200),
-@imagen VARCHAR(255),
-@id_categoria INT,
-@id_estado INT
-AS
-BEGIN
-    UPDATE Libros
-	SET titulo = @titulo, id_autor = @id_autor, id_editorial = @id_editorial, descripcion = @descripcion, 
-		imagen = @imagen, id_categoria = @id_categoria, id_estado = @id_estado
-	WHERE id_libro = @id_libro
-END;
-GO
-
---EXEC usp_updateLibro 1, '', '', '';
---GO
-
-CREATE PROCEDURE usp_deleteLibro
-@id_libro INT
+CREATE PROCEDURE usp_deleteProducto
+@id_producto int
 AS
 BEGIN
     DELETE
-	FROM Libros
-	WHERE id_libro=@id_libro
+	FROM Producto
+	WHERE id_producto=@id_producto
 END;
 GO
 
---EXEC usp_deleteLibro 1;
---GO
+EXEC usp_deleteProducto 3;
+GO
+
+------SP para tabla Pedido
+CREATE PROCEDURE usp_listaPedido
+AS
+BEGIN
+    SELECT id_pedido, nom_cli, num_mesa, detalle, precio, id_producto
+	FROM Pedido
+END;
+GO
+
+EXEC usp_listaPedido;
+GO
+
+CREATE PROCEDURE usp_obtenerPedidoPorId
+@id_pedido int
+AS
+BEGIN
+	SELECT id_pedido, nom_cli, num_mesa, detalle, precio, id_producto
+	FROM Pedido
+	WHERE id_pedido=@id_pedido
+END;
+GO
+
+EXEC usp_obtenerPedidoPorId 1;
+GO
+
+CREATE PROCEDURE usp_addPedido
+@nom_cli VARCHAR(80),
+@num_mesa int,
+@detalle VARCHAR(80),
+@precio decimal (10,2),
+@id_producto int
+AS
+BEGIN
+    INSERT INTO Pedido(nom_cli, num_mesa, detalle, precio, id_producto)
+	VALUES (@nom_cli, @num_mesa, @detalle, @precio, @id_producto)
+END;
+GO
+
+EXEC usp_addPedido 'Cliente3', 3, 'Pedido n° 3', 20.5, 1;
+GO
+
+CREATE PROCEDURE usp_updatePedido
+@id_pedido int,
+@nom_cli VARCHAR(80),
+@num_mesa int,
+@detalle VARCHAR(80),
+@precio decimal (10,2),
+@id_producto int
+AS
+BEGIN
+    UPDATE Pedido
+	SET nom_cli = @nom_cli, num_mesa = @num_mesa, detalle = @detalle, precio = @precio, id_producto = @id_producto
+	WHERE id_pedido = @id_pedido
+END;
+GO
+
+EXEC usp_updatePedido 3, 'Cliente n°3', 4, 'Pedido para cliente 3', 22.5, 2;
+GO
+
+CREATE PROCEDURE usp_deletePedido
+@id_pedido INT
+AS
+BEGIN
+    DELETE
+	FROM Pedido
+	WHERE id_pedido=@id_pedido
+END;
+GO
+
+EXEC usp_deletePedido 3;
+GO
